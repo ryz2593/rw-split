4224e848671e88b0a1da3d6a532a8340e53e34d0
ryz2593 <805445415@qq.com>
2019-04-10 15:56:14 +0800

    add

--- src/main/java/com/ryz2593/mapper/rws/DynamicDataSource.java
+++ src/main/java/com/ryz2593/mapper/rws/DynamicDataSource.java
@@ -7,7 +7,7 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
  * @date 2019/4/10
  * @desc
  */
-public class DynamicDataSouce extends AbstractRoutingDataSource {
+public class DynamicDataSource extends AbstractRoutingDataSource {
     /**
      * 在spring容器中查询对应key来应用为数据源（key一定是数据源的）
      * {@link #resolveSpecifiedLookupKey} method.
